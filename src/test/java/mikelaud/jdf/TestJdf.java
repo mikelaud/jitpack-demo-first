f7b69fdfa67655a7f3620bc5c5190c02c7fd7dd1
Mike (Mykhailo) Oksenenko <mmx.mmx@gmail.com>
2017-07-09 03:25:22 +0300

    add test

---
+++ src/test/java/mikelaud/jdf/TestJdf.java
@@ -0,0 +1,14 @@
+package mikelaud.jdf;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestJdf {
+
+	@Test
+	public void test() {
+		assertTrue(JitpackDemoFirst.exec());
+	}
+	
+}
